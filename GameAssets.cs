09b5ced8191250d81c44cade5e2577e9c4899dc6
Tom Pohorily <sebastianbelle074@gmail.com>
2026-05-02 21:17:47 -0500

    refactor: delete old GameAssets file

--- GameAssets.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections.Generic;
-using Microsoft.Xna.Framework.Graphics;
-using ReLogic.Content;
-using Terraria;
-using Terraria.GameContent.UI.Elements;
-
-namespace ValkyrieLib;
